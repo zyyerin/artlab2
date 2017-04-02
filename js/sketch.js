fa2f7efb90f6237c8a8e7cabded0d8c15185641b
Erin Zhang <yzhang3@cca.edu>
2017-04-02 10:25:18 -0700

    p5 sketch update

--- js/sketch.js
+++ js/sketch.js
@@ -1,32 +1,39 @@
 var canvas;
 
-const PATH = 'data/', FILE = 'image', EXT = '.png';
-const IMAGES = 4, imgs = Array(IMAGES);
-var idx = 0;
+const PATH = 'js/data/', FILE = 'image', EXT = '.png';
+const IMAGES = 39, imgs = Array(IMAGES);
+var idx;
 
 function preload() {
-    for (var i = 0; i != IMAGES; imgs[i] = loadImage(PATH + FILE + (i++) + EXT));
+    for (var i = 0; i < IMAGES; i++){
+        imgs[i] = loadImage(PATH + FILE + i + EXT);
+    }
 }
 
 function setup() {
+    // background(255,0,0);
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('sketch-holder');
     imageMode(CENTER);
-}
-
-function draw(){
-    if (mouseIsPressed) {
-    idx = ~~random(IMAGES);
-    console.log("!!!!!" + idx);
-    push();
-    translate(mouseX, mouseY);
-    rotate(random(-30, 30));
-    image(imgs[idx], 0, 0);
-    blendMode(blendMode(EXCLUSION));
-    pop();
-}
+    blendMode(EXCLUSION);
 
 }
+
+// function mouseWheel(event) {
+// function mouseDragged() {
+function mouseReleased() {
+        idx = Math.floor(random(imgs.length));
+        push();
+        translate(mouseX, mouseY);
+        rotate(random(-30, 30));
+        image(imgs[idx], 0, 0);
+        pop();
+}
+
+function mousePressed() {
+        tint(255,255);
+}
+
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
